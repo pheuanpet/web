757e5805c51a23e8cc304b86a1d1518f5a9a2386
wy@manaosoftware.com <wy@manaosoftware.com>
2025-05-08 20:57:35 +0700

    update structure

---
+++ hosting/src/components/ui/Icon.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { cn } from '../../libs/utils';
+
+interface IconProps extends React.HTMLAttributes<HTMLSpanElement> {
+  icon?: string;
+}
+
+const Icon = React.forwardRef<HTMLSpanElement, IconProps>(
+  ({ className, icon, ...props }, ref) => (
+    <span ref={ref} className={cn('w-6 h-6', className, icon)} {...props} />
+  )
+);
+Icon.displayName = 'Icon';
+
+export { Icon };
